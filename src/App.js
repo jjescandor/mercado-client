e2bbb02129ddd2d30213867ccedde7eaf1060db1
JJ Escandor <jj.p.escandor@gmail.com>
2022-04-21 21:07:21 -0400

    Upload functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      img: ""
+    }
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const bodyFormData = new FormData();
+    bodyFormData.append('name', event.target.name.value);
+    bodyFormData.append('image', event.target.image.files[0]);
+    this.uploadImg(bodyFormData);
+  }
+
+  uploadImg = async (img) => {
+    const config = {
+      headers: { "Content-Type": "multipart/form-data" },
+      baseURL: `http://localhost:3002/`,
+      method: "post",
+      data: img
+    }
+    const res = await axios(config, img);
+    console.log(res.data);
+
+  }
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit} encType="multipart/form-data">
+          <input type="text" name="name" placeholder='name of file' />
+          <input type="file" name="image" placeholder='Upload image here' />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
